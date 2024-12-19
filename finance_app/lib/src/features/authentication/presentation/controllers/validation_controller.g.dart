ddce68aa726cd001be817089f6a949c2ec5c2198
metinilgar <metinilgarmutlu@gmail.com>
2024-12-20 01:16:12 +0300

    Add login screen verification

---
+++ finance_app/lib/src/features/authentication/presentation/controllers/validation_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'validation_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$validationControllerHash() =>
+    r'2612d8cacd62950b75e57e0e032c9595acf566fc';
+
+/// See also [ValidationController].
+@ProviderFor(ValidationController)
+final validationControllerProvider =
+    AutoDisposeNotifierProvider<ValidationController, void>.internal(
+  ValidationController.new,
+  name: r'validationControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$validationControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ValidationController = AutoDisposeNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
